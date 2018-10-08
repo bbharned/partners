22f82be28b110ab1da308a4486bf617e3f8987a4
Bryan <bbharned@gmail.com>
2018-10-08 15:31:01 -0400

    unistalling devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-devise_for :users
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 root 'pages#index'
-
+#get 'login', to: 'devise/sessions#new'
 
 
 
