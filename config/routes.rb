711520f63b8154138397a911d3ad12719cbc643e
Bryan <bbharned@gmail.com>
2018-10-08 14:42:33 -0400

    fixing conflicts after merge

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
 
-=======
-  devise_for :users
->>>>>>> devise
+devise_for :users
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 root 'pages#index'
