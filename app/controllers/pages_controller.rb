220ae120423c415dc2bbdf0d786ee72ee1f8d9a8
Bryan <bbharned@gmail.com>
2018-10-08 15:51:04 -0400

    removed devise

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,5 @@
 class PagesController < ApplicationController
 
-def index
-	
-end
-
 
 def dashboard
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,7 @@ Rails.application.routes.draw do
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 
-
-root 'pages#index'
-#get 'login', to: 'devise/sessions#new'
+root 'pages#dashboard'
 
 
 
