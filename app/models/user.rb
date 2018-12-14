d7b17e8a511014cf2979545fd53fe1f35cc506b4
Bryan <bbharned@gmail.com>
2018-12-14 10:50:32 -0500

    pr changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
 	attr_accessor :remember_token, :reset_token
 	validates :firstname, presence: true, length: { minimum: 1, maximum: 20 }
-    validates :lastname, presence: true, length: { minimum: 2, maximum: 30 }
+    validates :lastname, presence: true, length: { minimum: 1, maximum: 30 }
     validates :company, presence: true, length: { minimum: 2, maximum: 60 }
     validates :password, confirmation: true, :on => :create
     validates :email, confirmation: true, :on => :create
