4b9865149c1f3e7e841b1d5daf362e825c7629cf
Bryan <bbharned@gmail.com>
2020-05-11 19:49:49 -0400

    flex this sucka

--- app/controllers/flexforwards_controller.rb
+++ app/controllers/flexforwards_controller.rb
@@ -3,7 +3,6 @@ class FlexforwardsController < ApplicationController
 	before_action :require_admin, only: [:index]
 	before_action :set_flex, only: [:edit, :update, :show, :destroy]
 	before_action :require_same_user, only: [:edit, :update, :show, :destroy]
-    #before_action :calcs, only: [:create, :update]
 
 
 
@@ -83,89 +82,42 @@ end
 
 private
 
-def must_login
-	if !logged_in?
-        redirect_to login_path
+    def must_login
+    	if !logged_in?
+            redirect_to login_path
+        end
     end
-end
 
 
-def flex_params
-    params.require(:flexforward).permit(:name, :user_id, :currency_id, :mirrored, :red_exchange, :ex_serv_sup, :ex_serv_nosup, :ex_serv_nosup_years, :ex_site_sup, :ex_site_nosup, :ex_site_nosup_years, :ex_simp_sup, :ex_simp_nosup, :ex_simp_nosup_years, :ex_red_sup, :ex_red_nosup, :ex_red_nosup_years, :tr_serv, :tr_pr_serv, :tr_cred_serv, :tr_site, :tr_pr_site, :tr_cred_site, :tr_simp, :tr_pr_simp, :tr_cred_simp, :tr_red, :tr_pr_red, :tr_cred_red, :new_simp, :new_pr_simp, :new_red, :new_pr_red, :total_terms, :tr_pr_total, :tr_cred_total, :total_tr_cost, :total_maint, :sm_exp, :total_quote, :note)
-end
-
-
-def set_flex
-    @flex = Flexforward.find(params[:id])
-end
-
-
-def require_admin
-    if (logged_in? and !current_user.admin?) || !logged_in? 
-        flash[:danger] = "Only admin users can perform that action"
-        redirect_to root_path
+    def flex_params
+        params.require(:flexforward).permit(:name, :user_id, :currency_id, :mirrored, :red_exchange, :ex_serv_sup, :ex_serv_nosup, :ex_serv_nosup_years, :ex_site_sup, :ex_site_nosup, :ex_site_nosup_years, :ex_simp_sup, :ex_simp_nosup, :ex_simp_nosup_years, :ex_red_sup, :ex_red_nosup, :ex_red_nosup_years, :tr_serv, :tr_pr_serv, :tr_cred_serv, :tr_site, :tr_pr_site, :tr_cred_site, :tr_simp, :tr_pr_simp, :tr_cred_simp, :tr_red, :tr_pr_red, :tr_cred_red, :new_simp, :new_pr_simp, :new_red, :new_pr_red, :total_terms, :tr_pr_total, :tr_cred_total, :total_tr_cost, :total_maint, :sm_exp, :total_quote, :note)
     end
-end
 
 
-def require_same_user
-    if current_user != @flex.user && !current_user.admin
-        flash[:danger] = "You are not permitted for that action"
-        redirect_to root_path
+    def set_flex
+        @flex = Flexforward.find(params[:id])
     end
-end
 
 
-def set_flex
-    @flex = Flexforward.find(params[:id])
-end
+    def require_admin
+        if (logged_in? and !current_user.admin?) || !logged_in? 
+            flash[:danger] = "Only admin users can perform that action"
+            redirect_to root_path
+        end
+    end
 
 
+    def require_same_user
+        if current_user != @flex.user && !current_user.admin
+            flash[:danger] = "You are not permitted for that action"
+            redirect_to root_path
+        end
+    end
 
 
-
-# Flex Forward Calculations
-# def findRange(terminalCount)
-    
-#     if terminalCount == 0..5 
-#         terminalRange = 0
-#     elsif terminalCount >= 5 && terminalCount < 50 
-#         terminalRange = 1
-#     elsif terminalCount >= 50 && terminalCount < 100
-#         terminalRange = 2
-#     elsif terminalCount >= 100 && terminalCount < 250
-#         terminalCount = 3
-#     elsif terminalCount >= 250 && terminalCount < 500
-#         terminalRange = 4
-#     elsif terminalCount >= 500
-#         terminalRange = 5  
-#     end
-
-#     return terminalRange
-
-# end
-
-
-# def calcs
-    
-#     vfRedPrices = [3400, 1870, 1700, 1360, 1190, 1020]
-#     smrPrices = [400, 220, 200, 160, 140, 120]
-#     vfNonRedPrices = [2400, 1320, 1200, 960, 840, 720]
-
-#     @flex.tr_simp = @flex.ex_simp_sup + @flex.ex_simp_nosup
-#     @flex.tr_red = @flex.ex_red_sup + @flex.ex_red_nosup
-
-#     @flex.tr_pr_serv = (600 * @flex.currency.rate) * @flex.tr_serv
-#     @flex.tr_pr_site = (600 * @flex.currency.rate) * @flex.tr_site
-
-#     @flex.total_terms = @flex.tr_serv + @flex.tr_site + @flex.tr_simp + @flex.tr_red + @flex.new_simp + @flex.new_red
-
-#     #Ranges
-#     currentRange = findRange(@flex.total_terms)
-#     tradePackRange = findRange(@flex.tr_simp)
-#     tradeRPackRange = findRange(@flex.tr_red);
-    
-# end
+    def set_flex
+        @flex = Flexforward.find(params[:id])
+    end
 
 
 end
--- app/models/flexforward.rb
+++ app/models/flexforward.rb
@@ -5,27 +5,27 @@ class Flexforward < ApplicationRecord
 
 
 
-
-
 def findRange(terminalCount)
 
-    if terminalCount == 0..5 
-        terminalRange = 0
-    elsif terminalCount >= 5 && terminalCount < 50 
-        terminalRange = 1
-    elsif terminalCount >= 50 && terminalCount < 100
-        terminalRange = 2
-    elsif terminalCount >= 100 && terminalCount < 250
-        terminalCount = 3
-    elsif terminalCount >= 250 && terminalCount < 500
-        terminalRange = 4
-    elsif terminalCount >= 500
-        terminalRange = 5  
-    end
-
-    return terminalRange
-
+    if (0..4).include? terminalCount 
+        @terminalRange = 0
+    elsif (5..49).include? terminalCount 
+        @terminalRange = 1
+    elsif (50..99).include? terminalCount
+        @terminalRange = 2
+    elsif (100..249).include? terminalCount
+        @terminalRange = 3
+    elsif (250..499).include? terminalCount
+        @terminalRange = 4
+    elsif (terminalCount >= 500)
+        @terminalRange = 5 
+    end 
+    return @terminalRange
+    
 end
+
+
+
 
 
 
@@ -38,24 +38,108 @@ private
 
 # Flex Forward Calculations
 def calcs
-    
-    vfRedPrices = [3400, 1870, 1700, 1360, 1190, 1020]
-    smrPrices = [400, 220, 200, 160, 140, 120]
-    vfNonRedPrices = [2400, 1320, 1200, 960, 840, 720]
 
-    self.tr_simp = self.ex_simp_sup + self.ex_simp_nosup
-    self.tr_red = self.ex_red_sup + self.ex_red_nosup
+    @vfRedPrices = [3400, 1870, 1700, 1360, 1190, 1020]
+    @smrPrices = [400, 220, 200, 160, 140, 120]
+    @vfNonRedPrices = [2400, 1320, 1200, 960, 840, 720]
 
-    self.tr_pr_serv = (600 * self.currency.rate) * self.tr_serv
-    self.tr_pr_site = (600 * self.currency.rate) * self.tr_site
+    self.tr_simp = self.ex_simp_sup + self.ex_simp_nosup #15
+    self.tr_red = self.ex_red_sup + self.ex_red_nosup #16
 
-    self.total_terms = self.tr_serv + self.tr_site + self.tr_simp + self.tr_red + self.new_simp + self.new_red
+    self.total_terms = self.tr_serv + self.tr_site + self.tr_simp + self.tr_red + self.new_simp + self.new_red #19
 
     #Ranges
-    currentRange = findRange(self.total_terms)
-    tradePackRange = findRange(self.tr_simp)
-    tradeRPackRange = findRange(self.tr_red);
+    @currentRange = self.findRange(self.total_terms)
+    @tradePackRange = self.findRange(self.tr_simp)
+    @tradeRPackRange = self.findRange(self.tr_red)
 
+    #Prices
+    @simplexPricePer = @vfNonRedPrices[@currentRange] * self.currency.rate
+    @redundPricePer = @vfRedPrices[@currentRange] * self.currency.rate
+    @tradeSimplexPricePer = @vfNonRedPrices[@tradePackRange] * self.currency.rate
+    @tradeRedPricePer = @vfRedPrices[@tradeRPackRange] * self.currency.rate
+
+    #Trade up pricing
+    self.tr_pr_serv = (600 * self.currency.rate) * self.tr_serv #20
+    self.tr_pr_site = (600 * self.currency.rate) * self.tr_site #21
+    self.new_pr_simp = @simplexPricePer * self.new_simp #24
+    self.new_pr_red = @redundPricePer * self.new_red #25
+    self.tr_pr_simp = @tradeSimplexPricePer * self.tr_simp #22
+    self.tr_pr_red = @tradeRedPricePer * self.tr_red #23
+    self.tr_pr_total = self.tr_pr_serv + self.tr_pr_site + self.tr_pr_simp + self.tr_pr_red + self.new_pr_simp + self.new_pr_red #26
+
+    #Credits
+    ## Server
+    @creditForServerLicensesWithSupport = 61150 * self.ex_serv_sup * self.currency.rate
+    @creditForServerLicensesNoSupport = (1-(0.2*self.ex_serv_nosup_years))*(self.ex_serv_nosup*61150 * self.currency.rate)
+    if @creditForServerLicensesNoSupport < 0 
+        @creditForServerLicensesNoSupport = 0
+    end
+    self.tr_cred_serv = @creditForServerLicensesWithSupport + @creditForServerLicensesNoSupport
+    if self.tr_cred_serv < 0
+        self.tr_cred_serv = 0
+    end
+    if self.tr_cred_serv > self.tr_pr_serv 
+        self.tr_cred_serv = self.tr_pr_serv 
+    end
+
+    ## Site
+    @creditForSiteLicensesWithSupport = 134000 * self.ex_site_sup * self.currency.rate
+    @creditForSiteLicensesNoSupport = (1-(0.2*self.ex_site_nosup_years))*(self.ex_site_nosup*134000 * self.currency.rate)
+    if @creditForSiteLicensesNoSupport < 0 
+        @creditForSiteLicensesNoSupport = 0
+    end
+    self.tr_cred_site = @creditForSiteLicensesWithSupport + @creditForSiteLicensesNoSupport
+    if self.tr_cred_site < 0
+        self.tr_cred_site = 0
+    end
+    if self.tr_cred_site > self.tr_pr_site 
+        self.tr_cred_site = self.tr_pr_site 
+    end
+
+    ## Simplex
+    @creditForPackLicensesWithSupport = self.ex_simp_sup * @tradeSimplexPricePer
+    @creditForPackLicensesNoSupport = (1-(0.2*self.ex_simp_nosup_years))*(self.ex_simp_nosup*@tradeSimplexPricePer)
+    if @creditForPackLicensesNoSupport < 0
+        @creditForPackLicensesNoSupport = 0
+    end
+    self.tr_cred_simp = @creditForPackLicensesWithSupport + @creditForPackLicensesNoSupport #No negative
+    if self.tr_cred_simp < 0
+        self.tr_cred_simp = 0
+    end
+
+    if self.tr_cred_simp > self.tr_pr_simp
+        self.tr_cred_simp = self.tr_pr_simp
+    end
+
+    ## Redundant
+    @creditForRPackLicensesWithSupport = self.ex_red_sup * @tradeRedPricePer
+    @creditForRPackLicensesNoSupport = (1-(0.2*self.ex_red_nosup_years))*(self.ex_red_nosup*@tradeRedPricePer)
+    if @creditForRPackLicensesNoSupport < 0
+        @creditForRPackLicensesNoSupport = 0
+    end
+    self.tr_cred_red = @creditForRPackLicensesWithSupport + @creditForRPackLicensesNoSupport  #No negative
+    if self.tr_cred_red < 0
+        self.tr_cred_red = 0
+    end
+
+    if self.tr_cred_red > self.tr_pr_red
+        self.tr_cred_red = self.tr_pr_red
+    end
+
+    self.tr_cred_total = self.tr_cred_serv + self.tr_cred_site + self.tr_cred_simp + self.tr_cred_red;
+    if self.tr_cred_total < 0
+       self.tr_cred_total = 0
+    end
+
+    self.total_tr_cost = self.tr_pr_total - self.tr_cred_total
+
+    @termsForSM = self.tr_serv + self.tr_site + self.tr_simp + self.tr_red
+    @smrange = findRange(@termsForSM)
+    @smPricePer = @smrPrices[@currentRange] #This is the total term range
+    self.total_maint = @smPricePer * @termsForSM
+
+    self.total_quote = self.total_tr_cost + self.total_maint
 
 
 end
