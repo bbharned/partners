d8b7fc2b57e416201f84c16bbd7196daff260403
Bryan <bbharned@gmail.com>
2020-09-25 12:22:12 -0400

    Upgrade to rails 5.1.7

--- Gemfile
+++ Gemfile
@@ -8,7 +8,7 @@ end
 
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails', '~> 5.0.7'
+gem 'rails', '~> 5.1.7'
 
 
 gem 'bootstrap', '~> 4.3.1'
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,13 +18,15 @@ Rails.application.configure do
 
     config.cache_store = :memory_store
     config.public_file_server.headers = {
-      'Cache-Control' => 'public, max-age=172800'
+      'Cache-Control' => "public, max-age=#{2.days.seconds.to_i}"
     }
   else
     config.action_controller.perform_caching = false
 
     config.cache_store = :null_store
   end
+
+  #config.action_controller.raise_on_unfiltered_parameters = true
 
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
