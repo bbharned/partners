17834cd97277ecd9291803d9fe08a1be6491f72b
Bryan <bbharned@gmail.com>
2020-09-29 10:59:06 -0400

    upgrade to 5.2.4.4

--- Gemfile
+++ Gemfile
@@ -8,7 +8,7 @@ end
 
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails', '~> 5.1.7'
+gem 'rails', '~> 5.2.4.4'
 
 
 gem 'bootstrap', '~> 4.3.1'
--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,8 @@ module Partners
     config.load_defaults 5.1
 
     # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
   end
 end
