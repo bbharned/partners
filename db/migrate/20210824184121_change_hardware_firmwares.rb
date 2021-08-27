1ced886c23fa27d9e86861ed9145671194c41133
Bryan <bbharned@gmail.com>
2021-08-27 08:51:03 -0400

    fixing migration files

--- db/migrate/20210824184121_change_hardware_firmwares.rb
+++
@@ -1,6 +0,0 @@
-class ChangeHardwareFirmwares < ActiveRecord::Migration[5.2]
-  def change
-    change_column :hardwares, :min_firmware, :number
-    change_column :hardwares, :max_firmware, :number
-  end
-end
--- db/migrate/20210824195139_change_firmwares.rb
+++
@@ -1,5 +0,0 @@
-class ChangeFirmwares < ActiveRecord::Migration[5.2]
-  def change
-    change_column :firmwares, :version, :number
-  end
-end
