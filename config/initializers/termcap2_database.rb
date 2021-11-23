f68ea93c9bbea7d18ec63af97d0c80c6aeccca00
Bryan <bbharned@gmail.com>
2021-11-23 09:33:35 -0500

    env db connection

--- config/initializers/termcap2_database.rb
+++ config/initializers/termcap2_database.rb
@@ -1,1 +1,6 @@
-TERMCAP2 = YAML.load_file(File.join(Rails.root, "config", "termcap2.yml"))[Rails.env.to_s]
+#TERMCAP2 = YAML.load_file(File.join(Rails.root, "config", "termcap2.yml"))[Rails.env.to_s]
+
+file = File.read(Rails.root.join("config", "termcap2.yml"))
+yaml = ERB.new(file).result
+
+TERMCAP2 = YAML.load(yaml)[Rails.env.to_s]
