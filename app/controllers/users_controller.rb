f399a48a8311f27e2b43f53dcffe72a1009d38ba
Bryan <bbharned@gmail.com>
2022-01-04 11:19:55 -0500

    user profile additions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -327,7 +327,7 @@ end
 private
 
 	def user_params
-        params.require(:user).permit(:firstname, :lastname, :email, :email_confirmation, :company, :password, :password_confirmation, :continent, :active, :prttype, :silevel, :channel, :certdate, :certexpire, :search, :distributor, :integrator, :end_user, :oem, :needs_review, :hw_admin, :evt_admin)
+        params.require(:user).permit(:firstname, :lastname, :email, :email_confirmation, :company, :password, :password_confirmation, :continent, :active, :prttype, :silevel, :channel, :certdate, :certexpire, :search, :distributor, :integrator, :end_user, :oem, :needs_review, :hw_admin, :evt_admin, :cell, :carrier, :street, :street2, :city, :state, :zip)
     end
 
     def set_user
