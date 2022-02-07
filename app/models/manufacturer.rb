a91bacf7d885201d1b33362f07f96c100ff6de6e
Bryan <bbharned@gmail.com>
2022-02-07 15:25:28 -0500

    manufacturer model tweak

--- app/models/manufacturer.rb
+++ app/models/manufacturer.rb
@@ -3,7 +3,7 @@ class Manufacturer < Termcap2
 	has_many :terminals, class_name: 'Terminal'
 	
 	def self.options_for_select
-      order(Arel.sql("LOWER(Name)")).map { |e| [e.Name, e.id] }
+      order(Arel.sql("Name")).map { |e| [e.Name, e.id] }
     end
 
 end
