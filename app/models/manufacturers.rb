b182ffb29e0241c74cbf151333e70fd393c69919
Bryan <bbharned@gmail.com>
2022-02-08 10:01:51 -0500

    manufacture model changes

--- app/models/manufacturers.rb
+++ app/models/manufacturers.rb
@@ -1,9 +1,11 @@
 class Manufacturers < Termcap2
 	self.table_name = "Manufacturers"
 	has_many :terminals, class_name: :Terminal
+
+	@manufacturers = self.all
 	
 	def self.options_for_select
-	  self.order('Name asc').map { |e| [e.Name, e.Id] }
+	  @manufacturers.order('Name asc').map { |e| [e.Name, e.Id] }
 	end
 
 end
