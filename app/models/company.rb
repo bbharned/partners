ea3d0ea657890f5cc3419d18524c4a3b9290994e
Bryan <bbharned@gmail.com>
2022-03-22 11:58:59 -0400

    company creation

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,15 +1,11 @@
 class Company < ActiveRecord::Base
 	geocoded_by :address 
-	after_validation :geocode
+	after_validation :geocode, if: :address_changed?
   before_save :find_country_code
   
   def address 
     [street, state, country].compact.join(", ") 
   end
-
-  def address_changed? 
-    street_changed?||city_changed?||postal_code_changed?||state_changed?||country_changed?
-  end 
 
   def find_country_code
     if self.country_code == nil && self.latitude != nil && self.longitude != nil
@@ -17,6 +13,16 @@ class Company < ActiveRecord::Base
       self.country_code = query.first.country_code
     end
   end
+
+
+
+
+
+  private
+
+  def address_changed? 
+    street_changed?||city_changed?||postal_code_changed?||state_changed?||country_changed?
+  end 
 
 
 end
