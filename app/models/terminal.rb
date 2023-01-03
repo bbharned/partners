dc1c474102e0d8ae5345c3c94f361dc576653e9a
Bryan <bbharned@gmail.com>
2023-01-03 13:10:37 -0500

    hardware search problem

--- app/models/terminal.rb
+++ app/models/terminal.rb
@@ -47,7 +47,7 @@ scope :with_search_please, lambda { |query|
         )"
       }.join(' AND '),
       *terms.map { |e| [e] * num_or_conds }.flatten
-    ).includes(:Manufacturers).references(:ManufacturerId)
+    ).includes(:Manufacturers).references('Terminal.ManufacturerId')
 } 
 
 # scope :with_search_please, -> (search_string) {
