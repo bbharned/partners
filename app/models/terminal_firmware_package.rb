b616a50dd1cbbd3f87b7054d3cdcec5a21b3f53b
Bryan <bbharned@gmail.com>
2023-01-04 11:37:12 -0500

    small model changes

--- app/models/terminal_firmware_package.rb
+++ app/models/terminal_firmware_package.rb
@@ -1,7 +1,7 @@
 class TerminalFirmwarePackage < Termcap2
 	self.table_name = "TerminalFirmwarePackage"
 	self.primary_key = "Id"
-	belongs_to :terminal, class_name: :Terminal, foreign_key: :TerminalId
+	belongs_to :Terminal, class_name: :Terminal, foreign_key: :TerminalId
     belongs_to :FirmwarePackage, class_name: :FirmwarePackage, foreign_key: :PackageId
 
 
