d4b25ec5306a342f0d1ad45bfbe3f20ede9da53d
Bryan <bbharned@gmail.com>
2023-01-04 17:37:40 -0500

    attribute updates

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -74,7 +74,7 @@ def signup
   @user.cert_signup = true
     if @user.save
         session[:user_id] = @user.id
-        @user.update(:lastlogin, Time.now)
+        @user.update_attribute(:lastlogin, Time.now)
         @user.send_signup_notice  #change for production
         @user.send_newuser_zap
         @user.send_user_signup_notice #change for production
@@ -94,7 +94,7 @@ def signup_rau
   @user.referred_by = "RAU"
     if @user.save
         session[:user_id] = @user.id
-        @user.update(:lastlogin, Time.now)
+        @user.update_attribute(:lastlogin, Time.now)
         @user.send_rau_notice
         @user.send_newuser_zap 
         @user.send_user_signup_notice #change for production
@@ -172,7 +172,7 @@ def learn_signup
   @user.learn_signup = true
     if @user.save
         session[:user_id] = @user.id
-        @user.update(:lastlogin, Time.now)
+        @user.update_attribute(:lastlogin, Time.now)
         #@user.send_signup_notice  #change for production
         #user.send_newuser_zap
         #@user.send_user_signup_notice #change for production
