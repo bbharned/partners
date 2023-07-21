61df80dc4941473e9ea14a5009a564217b479a91
Bryan <bbharned@gmail.com>
2023-07-21 08:09:34 -0400

    training tweaks

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,7 +1,7 @@
 class Answer < ApplicationRecord
 	belongs_to :question
 	has_many :wrongs
-    validates :answer, presence: true, length: { minimum: 1, maximum: 100 }
+    validates :answer, presence: true, length: { minimum: 1, maximum: 300 }
 
 
 end
--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -8,7 +8,7 @@ class Quiz < ActiveRecord::Base
 	has_many :wrongs
 	has_many :user_quizzes
     has_many :users, through: :user_quizzes
-	validates :name, presence: true, length: { minimum: 5, maximum: 100 }
+	validates :name, presence: true, length: { minimum: 5, maximum: 200 }
 	validates_uniqueness_of :name
 	accepts_nested_attributes_for :questions
 
