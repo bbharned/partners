2fc3059dbaa5fafe0ed211ed20cc7715af2fc682
Bryan <bbharned@gmail.com>
2023-11-14 13:32:18 -0500

    cert renew button

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,7 @@ class SessionsController < ApplicationController
         if user && user.active && user.authenticate(params[:session][:password])
             session[:user_id] = user.id
             user.update_attribute(:lastlogin, Time.now)
-            if ((user.street == nil || user.street == "") || (user.city == nil || user.city == "") || (user.cell == nil || user.cell == "") || (user.carrier == nil || user.carrier == "")) && !user.admin?
+            if ((user.street == nil || user.street == "") || (user.city == nil || user.city == "")) && !user.admin?
                 flash[:warning] = "Your profile is incomplete. Update your information #{view_context.link_to 'here', edit_user_path(user)}."
             else 
                 flash[:success] = "You have successfully logged In"
