9e0b8f1ceaacfd4d53427a02045e32662a074071
Bryan <bbharned@gmail.com>
2024-06-24 15:09:23 -0400

    roi still going

--- app/models/roi.rb
+++ app/models/roi.rb
@@ -1,8 +1,93 @@
 class Roi < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :currency
-	#before_save :calcs
+	before_save :calcs
 	validates :name, presence: true, length: { minimum: 1, maximum: 50 }
+
+
+
+
+
+
+private
+
+
+
+
+def calcs
+######### Capital Costs #########
+# PC Calculations
+    @pcHardwareCosts = self.planned_terminals * self.pc_ave_cost
+    @pcInvCosts = self.pc_ave_cost * self.pc_inventory_count
+    self.result_capcost_pc =  @pcHardwareCosts + @pcInvCosts
+
+# TC Calculations
+    @tcHardwareCosts = self.planned_terminals * self.tc_ave_cost
+    @tcInvCosts = self.tc_ave_cost * self.tc_inventory_count
+
+#RDS Calculations
+	if self.planned_terminals % self.rds_sessions_number == 0
+		@rdserversneeded = self.planned_terminals / self.rds_sessions_number
+	else
+		@rdserversneeded = ((self.planned_terminals.to_f/self.rds_sessions_number.to_f).ceil).to_i
+	end
+	@rdservInvCosts = self.rds_ave_cost * self.rds_inventory_count
+	@rdsserverTotalCost = @rdserversneeded * self.rds_ave_cost
+
+#Capital cost totals
+	if self.activation_type != "Subscription" #Perpetual License
+		self.result_capcost_tc_rds = @tcHardwareCosts + @tcInvCosts + @rdservInvCosts + @rdsserverTotalCost + self.thinmanager_cost + self.thinmanager_redundancy 
+	else
+		self.result_capcost_tc_rds = @tcHardwareCosts + @tcInvCosts + @rdservInvCosts + @rdsserverTotalCost 
+	end
+	if self.activation_type != "Subscription" #Perpetual License
+		@subscriptionCost = 0
+	else
+		@subscriptionCost = (self.thinmanager_cost + self.thinmanager_redundancy) * self.projected_years
+	end
+
+#Capital Cost Savings
+	self.savings_capcost = self.result_capcost_pc - self.result_capcost_tc_rds
+
+
+
+
+############ Wages ###############
+self.base_labor_per_hour = self.base_labor_per_year / 52 / 40
+self.total_labor_per_year = self.base_labor_per_year * (self.labor_overhead / 100 + 1)
+self.total_labor_per_hour = self.total_labor_per_year / 52 / 40
+
+
+
+
+####### Preperation Costs #######
+@PCprepCosts = self.planned_terminals * self.pc_prep_time * self.total_labor_per_hour
+@PCInventoryPrepCosts = self.pc_inventory_count * self.pc_prep_time * self.total_labor_per_hour
+@TCprepCosts = self.planned_terminals * self.tc_prep_time * self.total_labor_per_hour
+@TCInventoryPrepCosts = self.tc_prep_time * self.tc_inventory_count * self.total_labor_per_hour
+@RDSprepCosts = self.rds_prep_time * self.total_labor_per_hour * @rdserversneeded
+@RDSInventoryPrepCosts = self.rds_inventory_count * self.rds_prep_time * self.total_labor_per_hour
+self.result_prepcost_pc = @PCprepCosts + @PCInventoryPrepCosts
+self.result_prepcost_tc_rds = @TCprepCosts + @TCInventoryPrepCosts + @RDSprepCosts + @RDSInventoryPrepCosts
+self.savings_prepcost = self.result_prepcost_pc - self.result_prepcost_tc_rds
+
+
+
+####### Operating Costs #######
+
+
+
+#self.savings_opcost = self.result_opcost_pc - self.result_opcost_tc_rds
+
+
+
+end
+
+
+
+
+
+
 
 
 
--- db/migrate/20240618175734_create_roi_table.rb
+++ db/migrate/20240618175734_create_roi_table.rb
@@ -10,74 +10,74 @@ class CreateRoiTable < ActiveRecord::Migration[6.1]
 
       #General
       t.integer :planned_terminals, default: 100
-      t.decimal :station_uptime, precision: 10, scale: 2, default: 16.0
-      t.decimal :kilowatt_power_cost, precision: 10, scale: 4, default: 0.1319
-      t.decimal :downtime_cost_hour, precision: 10, scale: 4, default: 10000.00
+      t.decimal :station_uptime, precision: 4, scale: 2, default: 16.0
+      t.decimal :kilowatt_power_cost, precision: 4, scale: 4, default: 0.1319
+      t.decimal :downtime_cost_hour, precision: 4, scale: 4, default: 10000.00
       t.integer :projected_years, default: 10
-      t.decimal :additional_savings, precision: 10, scale: 2, default: 0.00
+      t.decimal :additional_savings, precision: 4, scale: 2, default: 0.00
       t.text :additional_savings_note
 
       #Labor
-      t.decimal :base_labor_per_year, precision: 10, scale: 2, default: 50000.00
-      t.decimal :base_labor_per_hour, precision: 10, scale: 2, default: 24.04
-      t.decimal :labor_overhead, precision: 10, scale: 2, default: 52.00
-      t.decimal :total_labor_per_year, precision: 10, scale: 2, default: 76000.00
-      t.decimal :total_labor_per_hour, precision: 10, scale: 2, default: 36.54
+      t.decimal :base_labor_per_year, precision: 4, scale: 2, default: 50000.00
+      t.decimal :base_labor_per_hour, precision: 4, scale: 2, default: 24.04
+      t.decimal :labor_overhead, precision: 4, scale: 2, default: 52.00
+      t.decimal :total_labor_per_year, precision: 4, scale: 2, default: 76000.00
+      t.decimal :total_labor_per_hour, precision: 4, scale: 2, default: 36.54
 
       #PC Stats
-      t.decimal :pc_ave_cost, precision: 10, scale: 2, default: 1000.00
-      t.decimal :pc_prep_time, precision: 10, scale: 2, default: 4.00
-      t.decimal :pc_monthly_maint, precision: 10, scale: 2, default: 1.00
+      t.decimal :pc_ave_cost, precision: 4, scale: 2, default: 1000.00
+      t.decimal :pc_prep_time, precision: 4, scale: 2, default: 4.00
+      t.decimal :pc_monthly_maint, precision: 4, scale: 2, default: 1.00
       t.integer :pc_inventory_count, default: 5
-      t.decimal :pc_percent_fail_rate, precision: 10, scale: 2, default: 5.00
-      t.decimal :pc_watt_power_comsumption, precision: 10, scale: 2, default: 75.00
+      t.decimal :pc_percent_fail_rate, precision: 4, scale: 2, default: 5.00
+      t.decimal :pc_watt_power_comsumption, precision: 4, scale: 2, default: 75.00
       t.integer :pc_month_ave_life, default: 36
-      t.decimal :pc_ave_replace_time, precision: 10, scale: 2, default: 2.00
+      t.decimal :pc_ave_replace_time, precision: 4, scale: 2, default: 2.00
 
       #TC Stats
-      t.decimal :tc_ave_cost, precision: 10, scale: 2, default: 500.00
-      t.decimal :tc_prep_time, precision: 10, scale: 2, default: 0.00
-      t.decimal :tc_monthly_maint, precision: 10, scale: 2, default: 0.50
+      t.decimal :tc_ave_cost, precision: 4, scale: 2, default: 500.00
+      t.decimal :tc_prep_time, precision: 4, scale: 2, default: 0.00
+      t.decimal :tc_monthly_maint, precision: 4, scale: 2, default: 0.50
       t.integer :tc_inventory_count, default: 2
-      t.decimal :tc_percent_fail_rate, precision: 10, scale: 2, default: 1.00
-      t.decimal :tc_watt_power_comsumption, precision: 10, scale: 2, default: 15.00
+      t.decimal :tc_percent_fail_rate, precision: 4, scale: 2, default: 1.00
+      t.decimal :tc_watt_power_comsumption, precision: 4, scale: 2, default: 15.00
       t.integer :tc_month_ave_life, default: 60
-      t.decimal :tc_ave_replace_time, precision: 10, scale: 2, default: 0.25
+      t.decimal :tc_ave_replace_time, precision: 4, scale: 2, default: 0.25
 
       #RDS Stats
-      t.decimal :rds_ave_cost, precision: 10, scale: 2, default: 10000.00
-      t.decimal :rds_prep_time, precision: 10, scale: 2, default: 8.00
-      t.decimal :rds_monthly_maint, precision: 10, scale: 2, default: 2.00
+      t.decimal :rds_ave_cost, precision: 4, scale: 2, default: 10000.00
+      t.decimal :rds_prep_time, precision: 4, scale: 2, default: 8.00
+      t.decimal :rds_monthly_maint, precision: 4, scale: 2, default: 2.00
       t.integer :rds_sessions_number, default: 20
       t.integer :rds_inventory_count, default: 1
-      t.decimal :rds_watt_power_comsumption, precision: 10, scale: 2, default: 200.00
+      t.decimal :rds_watt_power_comsumption, precision: 4, scale: 2, default: 200.00
       t.integer :rds_month_ave_life, default: 60
-      t.decimal :rds_percent_fail_rate, precision: 10, scale: 2, default: 1.00
-      t.decimal :rds_uptime, precision: 10, scale: 2, default: 24.0
-      t.decimal :rds_ave_replace_time, precision: 10, scale: 2, default: 2.00
+      t.decimal :rds_percent_fail_rate, precision: 4, scale: 2, default: 1.00
+      t.decimal :rds_uptime, precision: 4, scale: 2, default: 24.0
+      t.decimal :rds_ave_replace_time, precision: 4, scale: 2, default: 2.00
 
       #ThinManager
-      t.decimal :thinmanager_cost, precision: 10, scale: 2, default: 0.00
-      t.decimal :thinmanager_redundancy, precision: 10, scale: 2, default: 0.00
-      t.decimal :thinmanager_maintenance, precision: 10, scale: 2, default: 0.00
-      t.decimal :management_time_saved_per_month, precision: 10, scale: 2, default: 0.25
+      t.decimal :thinmanager_cost, precision: 4, scale: 2, default: 0.00
+      t.decimal :thinmanager_redundancy, precision: 4, scale: 2, default: 0.00
+      t.decimal :thinmanager_maintenance, precision: 4, scale: 2, default: 0.00
+      t.decimal :management_time_saved_per_month, precision: 4, scale: 2, default: 0.25
 
       #results
-      t.decimal :result_capcost_pc, precision: 10, scale: 2
-      t.decimal :result_capcost_tc_rds, precision: 10, scale: 2
-      t.decimal :savings_capcost, precision: 10, scale: 2
-      t.decimal :result_prepcost_pc, precision: 10, scale: 2
-      t.decimal :result_prepcost_tc_rds, precision: 10, scale: 2
-      t.decimal :savings_prepcost, precision: 10, scale: 2
-      t.decimal :result_opcost_pc, precision: 10, scale: 2
-      t.decimal :result_opcost_tc_rds, precision: 10, scale: 2
-      t.decimal :savings_opcost, precision: 10, scale: 2
-      t.decimal :result_refreshcost_pc, precision: 10, scale: 2
-      t.decimal :result_refreshcost_tc_rds, precision: 10, scale: 2
-      t.decimal :savings_refreshcost, precision: 10, scale: 2
-      t.decimal :result_total_pc, precision: 10, scale: 2
-      t.decimal :result_total_tc_rds, precision: 10, scale: 2
-      t.decimal :savings_total, precision: 10, scale: 2
+      t.decimal :result_capcost_pc, precision: 4, scale: 2
+      t.decimal :result_capcost_tc_rds, precision: 4, scale: 2
+      t.decimal :savings_capcost, precision: 4, scale: 2
+      t.decimal :result_prepcost_pc, precision: 4, scale: 2
+      t.decimal :result_prepcost_tc_rds, precision: 4, scale: 2
+      t.decimal :savings_prepcost, precision: 4, scale: 2
+      t.decimal :result_opcost_pc, precision: 4, scale: 2
+      t.decimal :result_opcost_tc_rds, precision: 4, scale: 2
+      t.decimal :savings_opcost, precision: 4, scale: 2
+      t.decimal :result_refreshcost_pc, precision: 4, scale: 2
+      t.decimal :result_refreshcost_tc_rds, precision: 4, scale: 2
+      t.decimal :savings_refreshcost, precision: 4, scale: 2
+      t.decimal :result_total_pc, precision: 4, scale: 2
+      t.decimal :result_total_tc_rds, precision: 4, scale: 2
+      t.decimal :savings_total, precision: 4, scale: 2
 
       t.timestamps
     end
