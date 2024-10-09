c8c0aca44c1f22baa3ac488922e33247c6d9a046
Bryan <bbharned@gmail.com>
2024-10-09 14:43:58 -0400

    tablets page frame and function

--- app/assets/javascripts/tablet-pops.js
+++ app/assets/javascripts/tablet-pops.js
@@ -15,6 +15,34 @@ $( document ).on('turbolinks:load', function() {
 			$('.bs-second-canvas-right').addClass('mr-0');
 		return false;
 	});
+
+	$('.enable-card').on('click', function(){
+		$('body').prepend('<div class="bs-canvas-overlay position-fixed w-100 h-100"></div>');
+		if($(this).hasClass('enable-card'))
+			$('.bs-third-canvas-right').addClass('mr-0');
+		return false;
+	});
+
+	$('.choice-card').on('click', function(){
+		$('body').prepend('<div class="bs-canvas-overlay position-fixed w-100 h-100"></div>');
+		if($(this).hasClass('choice-card'))
+			$('.bs-fourth-canvas-right').addClass('mr-0');
+		return false;
+	});
+
+	$('.behavior-card').on('click', function(){
+		$('body').prepend('<div class="bs-canvas-overlay position-fixed w-100 h-100"></div>');
+		if($(this).hasClass('behavior-card'))
+			$('.bs-fifth-canvas-right').addClass('mr-0');
+		return false;
+	});
+
+	$('.expand-card').on('click', function(){
+		$('body').prepend('<div class="bs-canvas-overlay position-fixed w-100 h-100"></div>');
+		if($(this).hasClass('expand-card'))
+			$('.bs-sixth-canvas-right').addClass('mr-0');
+		return false;
+	});
 	
 
 
